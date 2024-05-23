0c9274462199d993bd063a6eebbd8169ba76ef98
Nguyen Huy Khiem <khiem.nguyen-huy@internal.banvien.com>
2024-05-23 10:37:19 +0700

    Update Exercise Array

--- 00_Array/Array_Practise.c
+++ 00_Array/Array_Practise.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-int main(){
-    printf("hello");
+int main(){  
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);        
+    }
+    int chan = 0, le = 0, tc = 0, tl = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] % 2 == 0){
+            ++chan;
+            tc += a[i];
+        }
+        else{
+            ++le;
+            tl += a[i];
+        }
+    }
+    
+    printf("%d\n%d\n%d\n%d", chan, le, tc, tl);
 
     return 0;
 }
